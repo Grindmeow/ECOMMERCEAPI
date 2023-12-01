350531ca3f424155421d8d61a285906fa61c9679
Grindmeow <thehumaneatermeow@gmail.com>
2023-12-01 08:30:42 +0545

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
-
+const jwt = require("jsonwebtoken");
 //Register
 
 router.post("/register", async (req, res) => {
 
   try {
     const hash = bcrypt.hashSync(req.body.password, 12);
+
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -33,6 +35,14 @@ router.post("/login", async (req, res) => {
 
     if (user) {
       const isPass = bcrypt.compareSync(password, user.password);
+      const accessToken = jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+        process.env.JWT_SEC,
+        { expiresIn: "3d" }
+      );
+
 
       if (isPass) return res.status(201).json({
         username,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
 
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
+//Update
+router.put("/:id", verifyTokenAndAuthorization, (req, res) => {
+
+})
+
 
 
 module.exports = router
